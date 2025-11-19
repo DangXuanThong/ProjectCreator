6bcd5e471928a30a51864401564b27438b376e09
Dang Xuan Thong <dangxuanthongvn@gmail.com>
2025-11-19 21:42:38 +0700

    Initial commit

---
+++ composeApp/src/jvmMain/kotlin/com/dangxuanthong/projectcreator/main.kt
@@ -0,0 +1,13 @@
+package com.dangxuanthong.projectcreator
+
+import androidx.compose.ui.window.Window
+import androidx.compose.ui.window.application
+
+fun main() = application {
+    Window(
+        onCloseRequest = ::exitApplication,
+        title = "Project Creator"
+    ) {
+        App()
+    }
+}
